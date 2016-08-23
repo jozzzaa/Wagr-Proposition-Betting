fc4e6879fccf5936f25011e6b0459463c7029446
Andrew Pratley <pratley.andrew@gmail.com>
2016-08-23 14:44:50 +1000

    put in some helpers and model methods

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,15 @@
 module UsersHelper
 
+  def current_user
+    self.find_by(id: session[:user_id])
+  end
+
   def logged_in?
-    if User.find_by(id: session[:user_id])
+    if self.find_by(id: session[:user_id])
       return true
     else
       return false
     end
   end
-
-  def current_user
-    User.find_by(id: session[:user_id])
-  end
-
+  
 end
